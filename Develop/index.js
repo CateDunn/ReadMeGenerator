a16f08aa396de43deaea4e0bd45c39a414fec488
Cate Dunn <60521577+CateDunn@users.noreply.github.com>
2020-04-17 14:51:35 -0400

    adding outline of ReadMe text

--- Develop/index.js
+++ Develop/index.js
@@ -28,8 +28,8 @@ function promptUser() {
       },
       {
         type: "input",
-        name: "name",
-        message: "What is the name of your project?"
+        name: "title",
+        message: "What is the title of your project?"
       },
       {
         type: "input",
@@ -68,28 +68,46 @@ function promptUser() {
 
   function generateReadMe(answers) {
     return `
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <title>Document</title>
-  </head>
-  <body>
-    <div class="jumbotron jumbotron-fluid">
-    <div class="container">
-      <h1 class="display-4">Hi! My name is ${answers.name}</h1>
-      <p class="lead">I am from ${answers.location}.</p>
-      <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-      <ul class="list-group">
-        <li class="list-group-item">My GitHub username is ${answers.github}</li>
-        <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
-      </ul>
-    </div>
-  </div>
-  </body>
-  </html>`;
+  # ${answers.title} Project 
+    By: ${answers.github}
+  
+  # Description:
+    ${answers.description}
+
+  # Table of Contents:
+    
+    * Installation 
+    * Usage
+    * License
+    * Contributing
+    * Tests
+
+
+  # Installation
+
+    ${answers.install}
+
+  # Usage
+
+    ${answers.using}
+
+
+  # License
+
+    ${answers.license}
+
+
+  # Contributing
+
+    ${answers.contributions}
+
+
+  # Tests
+
+    ${answers.tests}
+
+
+  `;
   }
   
 
