c392511071d0746592b3be11aaa8a1bac0362810
Cate Dunn <60521577+CateDunn@users.noreply.github.com>
2020-04-17 14:30:32 -0400

    writes literal text to ReadMe

--- Develop/index.js
+++ Develop/index.js
@@ -2,93 +2,109 @@
 const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
-// const api = require("./utils/api")
+const util = require("util");
+
 const writeFileAsync = util.promisify(fs.writeFile);
+// const api = require("./utils/api")
+
 
 
 function promptUser() {
     return inquirer.prompt([
       {
         type: "input",
-        name: "title",
-        message: "What is the title of your new project?"
+        name: "github",
+        message: "What is your GitHub username?"
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your email?"
+      },
+      {
+        type: "input",
+        name: "URL",
+        message: "Enter the URL of your project"
+      },
+      {
+        type: "input",
+        name: "name",
+        message: "What is the name of your project?"
       },
       {
         type: "input",
         name: "description",
-        message: "Write a brief description of your project"
+        message: "Write a short description of your project"
       },
       {
         type: "input",
-        name: "table of contents",
-        message: "Enter table of contents?"
+        name: "license",
+        message: "What kind of license should your project have?"
       },
       {
         type: "input",
-        name: "installation",
-        message: "What are the directions for installation?"
-      },
-      {
-        type: "input",
-        name: "contributing",
-        message: "Who will be contributing"
+        name: "install",
+        message: "What command should be run to install dependencies?"
       },
       {
         type: "input",
         name: "tests",
-        message: "??"
+        message: "What command should be run to run tests?"
       },
       {
         type: "input",
-        name: "github",
-        message: "What is your Github username?"
+        name: "using",
+        message: "What does the user need to know about using the repo?"
       },
       {
         type: "input",
-        name: "tests",
-        message: "What is your email address?"
+        name: "contributions",
+        message: "What does the user need to know about contributing to the repo?"
       },
       
-    ]).then(function(response){
-        console.log(response)
-        fs.writeFile("README.md", response, function(err) {
-
-            if (err) {
-              return console.log(err);
-            }
-          
-            console.log("Successfully created README!");
-          
-          });
-    })
+    ])
     
   };
+
+  function generateReadMe(answers) {
+    return `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <title>Document</title>
+  </head>
+  <body>
+    <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4">Hi! My name is ${answers.name}</h1>
+      <p class="lead">I am from ${answers.location}.</p>
+      <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+      <ul class="list-group">
+        <li class="list-group-item">My GitHub username is ${answers.github}</li>
+        <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
+      </ul>
+    </div>
+  </div>
+  </body>
+  </html>`;
+  }
   
 
-  promptUser();
-  
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  promptUser()
+    .then(function(answers) {
+        const readME = generateReadMe(answers)
+    
+        return writeFileAsync("README.md", readME);
+    })
+    .then(function() {
+        console.log("Successfully wrote to README.md");
+    })
+    .catch(function(err) {
+        console.log(err);
+    })
 
 
 
